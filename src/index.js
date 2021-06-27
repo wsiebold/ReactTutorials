1c014bf637560187bb2afed514f82678f26f29c8
wsiebold <email@designpdx.com>
2021-06-27 13:23:04 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>JSX Code Challenge</h1>
+    <ul>
+      <li>element 1</li>
+      <li>element 2</li>
+      <li>element 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
